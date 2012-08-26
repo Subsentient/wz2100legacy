2ebdddafe0b3c11032d7cb0b6bdad82a0d921734
Subsentient <thinkingrodent@gmail.com>
2012-08-25 20:31:54 -0700

    Reset us to microwave_popping and drop the netcode back down for that.

--- wzlegacy/src/autorevision.h
+++ wzlegacy/src/autorevision.h
@@ -5,7 +5,7 @@
 #define VCS_NUM			11566
 #define VCS_DATE		2012-08-19 00:00:00 +0700
 #define VCS_URI			
-#define VCS_TAG			microwave_08-25-2012
+#define VCS_TAG			microwave_popping
 
 #define VCS_FULL_HASH					e8a51a2ec434b683c938797cdb80612e0866c52e
 #define VCS_SHORT_HASH					e8a51a2
