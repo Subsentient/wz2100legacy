02ef5229c55c74141b94897f50e088fce28ed47d
Subsentient <thinkingrodent@gmail.com>
2012-10-23 01:54:47 -0700

    Remove old nullbot files.

--- wzlegacy/data/mp/multiplay/skirmish/nullbot-1-35-ge.js
+++
@@ -1,8 +0,0 @@
-
-const DEF_LIGHT = 0; // change 0 to 1 to get a weaker AI with lower CPU usage
-
-include("multiplay/skirmish/nullbot-1-35-head.inc.js");
-
-include("multiplay/skirmish/nullbot-1-35-ge.inc.js");
-
-include("multiplay/skirmish/nullbot-1-35-tail.inc.js");
