39f9176196f780cb83a9557c2c0ff5ad1ed3889e
Subsentient <thinkingrodent@gmail.com>
2012-11-02 23:17:30 -0700

    Remove build script for now, since it's been somewhat replaced by new features in ./configure

--- build_microwave_popping.sh
+++
@@ -1,7 +0,0 @@
-git pull
-rm -r ./wzlegacy_build
-cp -R ./wzlegacy ./wzlegacy_build
-cd ./wzlegacy_build
-./configure CFLAGS="-march=native -mtune=native -O3" CXXFLAGS="-march=native -mtune=native -O3"
-make -j4
-sudo make install
