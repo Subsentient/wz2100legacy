89680ef696cf5610f198cd81eb6c26f4956ef0b3
Subsentient <thinkingrodent@gmail.com>
2012-11-14 03:01:46 -0700

    Update us to nullbot 1.39

--- wzlegacy/data/mp/multiplay/skirmish/nullbot-1-38-mr.js
+++
@@ -1,8 +0,0 @@
-
-const DEF_LIGHT = 0; // change 0 to 1 to get a weaker AI with lower CPU usage
-
-include("multiplay/skirmish/nullbot-1-38-header.js.inc");
-
-include("multiplay/skirmish/nullbot-1-38-mr.js.inc");
-
-include("multiplay/skirmish/nullbot-1-38-main.js.inc");
