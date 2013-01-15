9de5cd68c576049bbf4fa58931d4f4798038fbae
Subsentient <thinkingrodent@gmail.com>
2013-01-15 04:31:39 -0700

    Make spectator code readable to rest of universe, don't change it's effects however.

--- wzlegacy/src/spectate.cpp
+++ wzlegacy/src/spectate.cpp
@@ -45,94 +45,143 @@ bool blockDebug = false; //Block debug mode? Used only for spectators.
 
 //Thread used by spectator support. -Subsentient
 
-int specThread(void *) { 
-  //Tiny function needed for delaying the minimap display for spectators, so if we have an HQ destroyed we still get a minimap. -Subsentient
-  //It needs to be in a thread so that it's actually useful, otherwise it hard freezes the game until it's time is up and the stucts are not destroyed yet.
-  //This also enables uplink-ish-ness and stuff.
+int specThread(void *)
+//Tiny function needed for delaying the minimap display for spectators, so if we have an HQ destroyed we still get a minimap. -Subsentient
+//It needs to be in a thread so that it's actually useful, otherwise it hard freezes the game until it's time is up and the stucts are not destroyed yet.
+//This also enables uplink-ish-ness and stuff.
+{
 
-  for(int tempgt = wzGetTicks(); apsStructLists[selectedPlayer] != NULL && apsDroidLists[selectedPlayer] != NULL; tempgt = tempgt + 1000) { //Just increment tempgt to be efficient.
-   while (wzGetTicks() < tempgt + 1000) { //We just keep waiting until everything is dead so we don't
-    wzYieldCurrentThread(); } } //spawn our minimap too soon and have it turned off when our HQ dies. -Subsentient
+	for(int tempgt = wzGetTicks(); apsStructLists[selectedPlayer] != NULL && apsDroidLists[selectedPlayer] != NULL; tempgt = tempgt + 1000) //Just increment tempgt to be efficient.
+	{
+		while (wzGetTicks() < tempgt + 1000) //We just keep waiting until everything is dead so we don't
+		{					//spawn our minimap too soon and have it turned off when our HQ dies. -Subsentient
+			wzYieldCurrentThread();
+		}
+	}
 
-  widgDelete(psWScreen, IDPOW_POWERBAR_T); //Deletes the power bar. -Subsentient
-  godMode = true;
-  revealAll(selectedPlayer);
-  setRevealStatus(true);
-  radarPermitted = true;
-  if (getDebugMappingStatus()) { //Disable debug mode if we are already in it. -Subsentient
-   sendProcessDebugMappings(false); }
-  return 0; }
+	widgDelete(psWScreen, IDPOW_POWERBAR_T); //Deletes the power bar. -Subsentient
+	godMode = true;
+	revealAll(selectedPlayer);
+	setRevealStatus(true);
+	radarPermitted = true;
+
+	if (getDebugMappingStatus()) //Disable debug mode if we are already in it. -Subsentient
+	{
+		sendProcessDebugMappings(false);
+	}
+
+	return 0; 
+}
 
 
 //Sends spectator signal to other clients, if everything is in order.
 
-void sendSpecSignal() {
- if (!isSpectating && (allowSpectating || !NetPlay.bComms)) { //Don't let us spam spectator. -Subsentient
-  if (bMultiPlayer) {
-   isSpectating = true;
+void sendSpecSignal()
+{
+	if (!isSpectating && (allowSpectating || !NetPlay.bComms)) //Don't let us spam spectator. -Subsentient
+	{
+		if (bMultiPlayer)
+		{
+			isSpectating = true;
 
-   NETbeginEncode(NETgameQueue(selectedPlayer), GAME_SPECMODE); {//Send the universal signal, "Make me a spectator, everyone." -Subsentient
-    NETuint32_t(&selectedPlayer); }
-   NETend(); }
+			NETbeginEncode(NETgameQueue(selectedPlayer), GAME_SPECMODE); //Send the universal signal, "Make me a spectator, everyone." -Subsentient
+			{
+				NETuint32_t(&selectedPlayer);
+			}
+			NETend();
+		}
+	
+		else
+		{
+			addConsoleMessage("You are not in a multiplayer game.", DEFAULT_JUSTIFY, SYSTEM_MESSAGE);
+		}
 
-  else {
-   addConsoleMessage("You are not in a multiplayer game.", DEFAULT_JUSTIFY, SYSTEM_MESSAGE); } }
+	}
 
- else if (!isSpectating && !allowSpectating) {
-  addConsoleMessage("Spectating is not enabled for this game.", DEFAULT_JUSTIFY, SYSTEM_MESSAGE); }
- else {
-   addConsoleMessage("You are already a spectator.", DEFAULT_JUSTIFY, SYSTEM_MESSAGE); } }
+	else if (!isSpectating && !allowSpectating)
+	{
+		addConsoleMessage("Spectating is not enabled for this game.", DEFAULT_JUSTIFY, SYSTEM_MESSAGE);
+	}
+	else
+	{
+		addConsoleMessage("You are already a spectator.", DEFAULT_JUSTIFY, SYSTEM_MESSAGE);
+	}
+}
 
 
 //Subsentient's way of handling the GAME_SPECMODE network signal.
 
 void recvSpecSignal(NETQUEUE queue) {
-    DROID *psCDroid, *psNDroid;
-    STRUCTURE *psCStruct, *psNStruct;
-    WZ_THREAD *minimapThread = NULL;
-    int otherGuy;
-    uint32_t newSpec;
-    char specmsg[256];
 
-    NETbeginDecode(queue, GAME_SPECMODE); {//Figure out who dun' it. -Subsentient
-     NETuint32_t(&newSpec); }
-    NETend();
+	DROID *psCDroid, *psNDroid;
+	STRUCTURE *psCStruct, *psNStruct;
+	WZ_THREAD *minimapThread = NULL;
+	int otherGuy;
+	uint32_t newSpec;
+	char specmsg[256];
 
-   if (newSpec != queue.index) { //Someone is pulling a funky, sender and new spectator don't match.
-    debug(LOG_ERROR, "Player %d (%s) has attempted to spectate player %d! This is probably cheating.", queue.index, getPlayerName(queue.index), newSpec);
-    if (NetPlay.isHost) {
-     debug(LOG_INFO, "Kicking player %d for attempting to cheat with spectator calls.", queue.index);
-     kickPlayer(queue.index, "Trying to cheat and remove people from the game isn't polite.", ERROR_KICKED); }
-     return; } //Now that we did what we should, we exit the function and don't carry out the order. -Subsentient
+	NETbeginDecode(queue, GAME_SPECMODE); //Figure out who dun' it. -Subsentient
+	{
+		NETuint32_t(&newSpec);
+	}
+	NETend();
 
-   if (NetPlay.isHost && !allowSpectating && NetPlay.bComms) { //Someone is trying to spectate in a game that doesn't permit it.
-     debug(LOG_INFO, "Player %d has sent a request to spectate, but spectating is disabled. That can only mean trying to cheat. Kicking.", queue.index);
-     kickPlayer(queue.index, "You modified the game to send a spectator signal, so you can probably do other nasty things. Bye.", ERROR_KICKED);
-     return; }
+	if (newSpec != queue.index) //Someone is pulling a funky, sender and new spectator don't match.
+	{
+		debug(LOG_ERROR, "Player %d (%s) has attempted to spectate player %d! This is probably cheating.", queue.index, getPlayerName(queue.index), newSpec);
+		if (NetPlay.isHost)
+		{
+			debug(LOG_INFO, "Kicking player %d for attempting to cheat with spectator calls.", queue.index);
+			kickPlayer(queue.index, "Trying to cheat and remove people from the game isn't polite.", ERROR_KICKED);
+		}
+		return; //Now that we did what we should, we exit the function and don't carry out the order. -Subsentient
+	}
 
-   NetPlay.players[newSpec].spectating = true; //The player is now spectating. Tell the netcode that. -Subsentient
+	if (NetPlay.isHost && !allowSpectating && NetPlay.bComms) //Someone is trying to spectate in a game that doesn't permit it.
+	{
+		debug(LOG_INFO, "Player %d has sent a request to spectate, but spectating is disabled. That can only mean trying to cheat. Kicking.", queue.index);
+		kickPlayer(queue.index, "You modified the game to send a spectator signal, so you can probably do other nasty things. Bye.", ERROR_KICKED);
+		return;
+	}
 
-   for(psCDroid=apsDroidLists[newSpec]; psCDroid; psCDroid=psNDroid) { //Destroy all droids for the new spectator. -Subsentient
-    psNDroid = psCDroid->psNext;
-    destroyDroid(psCDroid, gameTime);  }
+	NetPlay.players[newSpec].spectating = true; //The player is now spectating. Tell the netcode that. -Subsentient
 
-   for(psCStruct=apsStructLists[newSpec]; psCStruct; psCStruct=psNStruct) { //Now for structures. -Subsentient
-    psNStruct = psCStruct->psNext;
-    turnOffMultiMsg(true);
-    destroyStruct(psCStruct, gameTime);
-    turnOffMultiMsg(false); }
+	for(psCDroid=apsDroidLists[newSpec]; psCDroid; psCDroid=psNDroid) //Destroy all droids for the new spectator. -Subsentient
+	{
+		psNDroid = psCDroid->psNext;
+		destroyDroid(psCDroid, gameTime);
+	}
 
-   for (otherGuy = 0; otherGuy < MAX_PLAYERS; otherGuy++) {//Breaks alliances with everyone. -Subsentient
-    if (otherGuy != newSpec) {
-     breakAlliance(newSpec, otherGuy, true, false); } }
+	for(psCStruct=apsStructLists[newSpec]; psCStruct; psCStruct=psNStruct) //Now for structures. -Subsentient
+	{
+		psNStruct = psCStruct->psNext;
+		turnOffMultiMsg(true);
+		destroyStruct(psCStruct, gameTime);
+		turnOffMultiMsg(false);
+	}
 
-   if (isSpectating && newSpec == selectedPlayer) { //If we are the target, start our thread to initialize the spectator features. -Subsentient
-    minimapThread = wzThreadCreate(specThread, NULL);
-    wzThreadStart(minimapThread);
-    addConsoleMessage("You have entered spectator mode.", DEFAULT_JUSTIFY, SYSTEM_MESSAGE); }
-   else {
-    blockDebug = true;
-    strcpy(specmsg, getPlayerName(newSpec));
-    strcat(specmsg, " is now a spectator.");
-    addConsoleMessage(specmsg, DEFAULT_JUSTIFY, SYSTEM_MESSAGE); } }
+	for (otherGuy = 0; otherGuy < MAX_PLAYERS; otherGuy++) //Breaks alliances with everyone. -Subsentient
+	{
+		if (otherGuy != newSpec)
+		{
+			breakAlliance(newSpec, otherGuy, true, false);
+		}
+	}
+
+	if (isSpectating && newSpec == selectedPlayer) //If we are the target, start our thread to initialize the spectator features. -Subsentient
+	{
+		minimapThread = wzThreadCreate(specThread, NULL);
+		wzThreadStart(minimapThread);
+		addConsoleMessage("You have entered spectator mode.", DEFAULT_JUSTIFY, SYSTEM_MESSAGE);
+	}
+
+	else 
+	{
+		blockDebug = true;
+		strcpy(specmsg, getPlayerName(newSpec));
+		strcat(specmsg, " is now a spectator.");
+		addConsoleMessage(specmsg, DEFAULT_JUSTIFY, SYSTEM_MESSAGE); 
+	}
+
+}
 
