e4d590b0f48f9d9c17714a4a337132ccc27188d4
Subsentient <thinkingrodent@gmail.com>
2013-05-28 13:25:27 -0700

    Increase max zoom-out to 7500, much better appearance and control.

--- wzlegacy/src/display.h
+++ wzlegacy/src/display.h
@@ -192,9 +192,9 @@ extern void	setDesiredPitch(SDWORD pitch);
 #define MAX_PLAYER_X_ANGLE	(-1)
 #define MIN_PLAYER_X_ANGLE	(-60)
 
-#define MAXDISTANCE	(3500)
-#define MINDISTANCE	(500)
-#define START_DISTANCE	(2000)
+#define MAXDISTANCE	(7500)
+#define MINDISTANCE	(250)
+#define START_DISTANCE	(3400)
 #define START_HEIGHT (1500)
 
 #define CAMERA_PIVOT_HEIGHT (500)
