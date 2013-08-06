4f3dc708f7cfdc35f084a0c608b6d9c1b1f09d70
wei2912 <wei2912.supp0rt@gmail.com>
2013-08-06 21:35:02 +0800

    Add wiki format file.

---
+++ stats/format/wiki.py
@@ -0,0 +1,27 @@
+def italic(line):
+	return "''" + line + "''"
+
+def bold(line):
+	return "'''" + line + "'''"
+	
+def heading(line, level):
+	head = "=" * level
+	return head + " " + line + " " + head
+
+# Horizontal rule
+def hr():
+	return "---"
+	
+# Unordered list (bullet list)
+def ul(line, level):
+	head = "*" * level
+	return head + " " + line
+
+# Ordered list (numbered list)
+def ol(line, level):
+	head = "#" * level
+	return head + " " + line
+
+# Links (from <a href="link">)
+def a(line):
+	return "[" + line + "]"
